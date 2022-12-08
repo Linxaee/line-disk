a1b8fc892cdd6ce94b874f6298699b26d6bbbf5c
Linxae <702611663@qq.com>
2022-12-08 14:57:22 +0800

    🚬init: add element-plus and auto import

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
 import path from "path";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		eslintPlugin({
 			include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
+		}),
+		AutoImport({
+			resolvers: [ElementPlusResolver()],
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()],
 		}),
 	],
 	resolve: {
