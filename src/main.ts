3c1ebdd8c4b2cdf007194231b16f5822b85c095c
Linxae <702611663@qq.com>
2022-12-08 15:39:48 +0800

    🚬init(global): add normalize-css to init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
+import "normalize.css";
+import "@/assets/scss/init.scss";
 
 const app = createApp(App);
 
