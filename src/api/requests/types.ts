0d2f2e3be0551aef928c883a1677063e3880e855
Linxae <702611663@qq.com>
2022-12-28 14:02:10 +0800

    ✨feature(api): upload type LinFileItem

--- src/api/requests/types.ts
+++ src/api/requests/types.ts
@@ -18,20 +18,30 @@ type fileType = 1 | 2;
  * @interface 请求到的文件类型接口
  */
 export interface LinFileItem {
-	/**
-	 * @description 文件名
-	 */
+	// 文件id
+	fileId: number;
+	// 文件名
 	fileName: string;
-	/**
-	 * @description 文件类型
-	 */
-	type: fileType;
-	/**
-	 * @description 文件大小
-	 */
-	fileSize: string | number;
-	/**
-	 * @description 创建时间
-	 */
+	// 文件扩展名
+	fileExt: string;
+	// 文件HASH
+	fileMd5: string;
+	// 文件大小
+	fileSize: number;
+	// 文件类型
+	fileType: fileType;
+	// 文件路径
+	filePath: string;
+	// 文件状态
+	fileStatus: number;
+	// 创建时间
 	createTime: string;
+	// 上传时间
+	updateTime: string;
+	// 删除时间
+	deleteTime?: any;
+	// 父亲id
+	parentId?: number;
+	// 是否是回收记录
+	isRecycleRecord: number;
 }
